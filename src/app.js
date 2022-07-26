5662dbbe2688a6a2b9d7c38673d514947c05f153
Beatriz Mendes <bea.smendes98@gmail.com>
2022-07-26 14:46:07 +0100

    chore: add existing project

---
+++ src/app.js
@@ -0,0 +1,100 @@
+// we use stringify to inline an example XML document
+import pizzaDiagram from '../resources/pizza-collaboration.bpmn';
+import emptyDiagram from '../resources/empty.bpmn';
+import absenseRequestDiagram from '../resources/absence-request.bpmn';
+import ticketBookingDiagram from '../resources/ticket-booking.bpmn'
+import errorHandling from '../resources/error-handling.bpmn';
+import enforcingSLA from '../resources/enforcing-sla.bpmn';
+
+import Modeler from "bpmn-js/lib/Modeler";
+
+import TokenSimulationModule from "bpmn-js-token-simulation/lib/modeler";
+import ResetSimulation from "bpmn-js-token-simulation/lib/features/reset-simulation";
+import TokenSimulationSupportModule from "bpmn-js-token-simulation/lib/simulation-support";
+
+const diagramSelect = document.getElementById("choose-diagram");
+const toggleSimulation = document.getElementById("toggle-simulation");
+const toggleGuide = document.getElementById("toggle-guide");
+const zoomIn = document.getElementById("zoom-in");
+const zoomOut = document.getElementById("zoom-out");
+const resetZoom = document.getElementById("reset-zoom");
+
+const diagrams = {
+  pizza: pizzaDiagram,
+  "absence-request": absenseRequestDiagram,
+  "ticket-booking": ticketBookingDiagram,
+  "enforcing-sla": enforcingSLA,
+  "error-handling": errorHandling,
+  "": emptyDiagram
+}
+
+let isTokenSimulationActivated = false;
+
+var modeler = new Modeler({
+  container: '#canvas',
+  keyboard: {
+    bindTo: document
+  },
+  additionalModules: [
+    TokenSimulationModule,
+    ResetSimulation,
+    TokenSimulationSupportModule
+  ]
+  
+});
+
+// disable scroll canvas with wheel
+modeler.get("zoomScroll").toggle(false);
+
+const resetSimulation = modeler.get("resetSimulation"),
+      simulationSupport = modeler.get("simulationSupport");
+
+const run = async () => {
+  await importDiagram(emptyDiagram);
+};
+
+const importDiagram = async (diagram) => {
+  const { error, warnings } = await modeler.importXML(diagram);
+
+  if (error) {
+    throw error;
+  }
+
+  if (warnings.length) {
+    console.log(warnings);
+  }
+};
+
+const resetTokenSimulation = (value) => {
+  resetSimulation.resetSimulation();
+  simulationSupport.toggleSimulation(value);
+  isTokenSimulationActivated = value;
+}
+
+diagramSelect.addEventListener("change", async (e) => {
+  await importDiagram(diagrams[e.target.value]);
+
+  if(isTokenSimulationActivated) {
+    resetTokenSimulation(false);
+    toggleSimulation.checked = false;
+  }
+});
+
+toggleSimulation.addEventListener("click", () => {
+
+  if(isTokenSimulationActivated) {
+    resetTokenSimulation(false);
+
+  } else {
+    resetTokenSimulation(true);
+  }
+});
+
+zoomIn.addEventListener("click", () => modeler.get("zoomScroll").stepZoom(1));
+zoomOut.addEventListener("click", () => modeler.get("zoomScroll").stepZoom(-1));
+resetZoom.addEventListener("click", () => modeler.get("zoomScroll").reset());
+
+run().catch((err) => {
+  console.log(err);
+});
+
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'app.bundled.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.bpmn$/,
+        use: {
+          loader: 'raw-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: {
+          loader: 'raw-loader'
+        }
+      }
+    ]
+  }
+};
